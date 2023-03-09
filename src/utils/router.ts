2e9e972826fa88e68201f0ef5a4704b6119106b2
郭孔泉 <guokongquan.sh@chinaccs.cn>
2023-03-09 10:38:57 +0800

    fix: 修正添加query参数的时候ID获取异常，使用前置守卫把外部动态参数放入window.route.params，后续API动态接口可以拼接参数
    http://localhost:3000/#/chart/preview/792622755697790976?t=123
    动态请求参数中使用javascript: return window.route?.params?.t

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -159,7 +159,8 @@ export const fetchRouteParams = () => {
  */
  export const fetchRouteParamsLocation = () => {
   try {
-    return document.location.hash.split('/').pop() || ''
+    // 防止添加query参数的时候，解析ID异常
+    return document.location.hash.split('?')[0].split('/').pop() || ''
   } catch (error) {
     window['$message'].warning('查询路由信息失败，请联系管理员！')
     return ''
@@ -190,4 +191,4 @@ export const loginCheck = () => {
   } catch (error) {
     return false
   }
-} 
+}
