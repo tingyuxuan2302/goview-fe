efe56bca0df8fd2d89462851335ee70673a7b549
tnt group <dodu@live.cn>
2023-05-20 13:53:15 +0800

    feat: 添加天气分组图标

--- src/packages/components/Icons/index.ts
+++ src/packages/components/Icons/index.ts
@@ -1,4 +1,5 @@
 import MaterialLine from './MaterialLine'
 import Unicons from './Unicons'
+import Weather from './Weather'
 
-export const IconList = [...MaterialLine, ...Unicons]
+export const IconList = [...MaterialLine, ...Unicons, ...Weather]
