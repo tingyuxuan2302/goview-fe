78310dcd050c29a714eaec8aca1f65d53f01fa7d
tnt group <dodu@live.cn>
2023-05-27 13:21:17 +0800

    fix: 上次遗漏的修复 #e463b2cd

--- src/packages/components/Photos/Private/index.ts
+++ src/packages/components/Photos/Private/index.ts
@@ -22,7 +22,7 @@ const userPhotosList: ConfigType[] = getLocalStorage(StoreKey) || []
 const uploadFile = (callback: Function | null = null) => {
   const input = document.createElement('input')
   input.type = 'file'
-  input.accept = 'image/*' // 这里只允许图片类型
+  input.accept = '.png,.jpg,.jpeg,.gif' // 这里只允许部分图片类型
   input.onchange = async () => {
     if (!input.files || !input.files.length) return
     const file = input.files[0]
@@ -53,7 +53,7 @@ const addConfig = {
   chartFrame: ChartFrameEnum.STATIC,
   title: '点击上传图片',
   image: 'upload.png',
-  redirectComponent: 'Informations/Mores/Image', // 跳转组件路径规则：packageName/categoryName/componentKey
+  redirectComponent: `${ImageConfig.package}/${ImageConfig.category}/${ImageConfig.key}`, // 跳转组件路径规则：packageName/categoryName/componentKey
   disabled: true,
   configEvents: {
     // 点击上传事件
